0c540d946bbb755f74af6eeee0adb5dff248c94e
Renata <renata.equals@gmail.com>
2024-09-28 09:54:46 -0300

    update validation

--- src/main/java/br/com/testeapi/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/testeapi/projeto/controller/UsuarioController.java
@@ -1,22 +1,30 @@
 package br.com.testeapi.projeto.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.testeapi.projeto.model.Usuario;
 import br.com.testeapi.projeto.repository.IUsuario;
 import br.com.testeapi.projeto.service.UsuarioService;
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin("*")
@@ -38,12 +46,12 @@ public class UsuarioController {
 	}
 
 	@PostMapping
-	public ResponseEntity<Usuario> criarUsuario (@RequestBody Usuario usuario) {
+	public ResponseEntity<Usuario> criarUsuario (@Valid @RequestBody Usuario usuario) {
 		return ResponseEntity.status(201).body(usuarioService.criarUsuario(usuario));
 	}
 
 	@PutMapping
-	public ResponseEntity<Usuario>  atualizarUsuario (@RequestBody Usuario usuario) {
+	public ResponseEntity<Usuario>  atualizarUsuario (@Valid @RequestBody Usuario usuario) {
 		return ResponseEntity.status(202).body(usuarioService.editarUsuario(usuario));
 	}
 
@@ -55,5 +63,17 @@ public class UsuarioController {
 
 	}
 
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error)->{
+		String fieldName = ((FieldError) error).getField();
+		String errorMessage = error.getDefaultMessage();
+		errors.put(fieldName, errorMessage);
+		});
+		
+		return errors;
+	}
 
 }
--- src/main/java/br/com/testeapi/projeto/model/Usuario.java
+++ src/main/java/br/com/testeapi/projeto/model/Usuario.java
@@ -6,6 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
 
 @Entity
 @Table(name = "usuario")
@@ -16,19 +22,23 @@ public class Usuario {
 	@Column(name = "id")
 	private Integer id;
 
-	@Column(name = "login", length = 50, nullable = true)
+	@Column(name = "login", length = 50, nullable = false)
 	private String login;
-
-	//testar com nome_completo ou nome
-	@Column(name = "nome_completo", length = 200, nullable = true)
+	
+	@NotBlank (message = "Campo Nome é obrigatório.")
+	@Column(name = "nome_completo", length = 200, nullable = false)
+	@Size(min=3, message = "O nome deve possuir, no mínimo, 3 caracteres.")
 	private String nome;
 
-	@Column(name = "email", length = 50, nullable = true)
+	@NotBlank (message = "Campo E-mail é obrigatório.")
+	@Column(name = "email", length = 50, nullable = false)
 	private String email;
 
-	@Column(name = "senha", columnDefinition = "TEXT", nullable = true)
+	@NotBlank (message = "Campo Senha é obrigatório.")
+	@Column(name = "senha", columnDefinition = "TEXT", nullable = false)
 	private String senha;
 
+	@NotBlank (message = "Campo Telefone é obrigatório.")
 	@Column(name = "telefone", length = 15, nullable = true)
 	private String telefone;
 
